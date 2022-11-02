9ce894de5ef90c1a7c6678f465d9b0235ef652ef
Wilson Santos <contato@wilsonsantos.net>
2022-11-02 20:09:52 -0300

    :sparkles: Atualiza algumas referencias e add libs

--- GeradorApiDDD-winx64.bat
+++ GeradorApiDDD-winx64.bat
@@ -24,6 +24,7 @@ dotnet sln add %1.Repository/%1.Repository.csproj
 dotnet sln add %1.Service/%1.Service.csproj
 
 dotnet add %1.Api/%1.Api.csproj reference %1.Domain/%1.Domain.csproj
+dotnet add %1.Api/%1.Api.csproj reference %1.IoC/%1.IoC.csproj
 
 dotnet add %1.CrossCutting/%1.CrossCutting.csproj reference %1.Domain/%1.Domain.csproj
 dotnet add %1.CrossCutting/%1.CrossCutting.csproj reference %1.Repository/%1.Repository.csproj
@@ -38,12 +39,15 @@ dotnet add %1.Testes/%1.Testes.csproj reference %1.Domain/%1.Domain.csproj
 dotnet add %1.Testes/%1.Testes.csproj reference %1.Api/%1.Api.csproj
 
 cd %1.Api
+dotnet add package Microsoft.EntityFrameworkCore.Design
 mkdir Controllers
 cd ..
 
 cd %1.CrossCutting
 dotnet add package AutoMapper
 dotnet add package Microsoft.Extensions.DependencyInjection.Abstractions
+dotnet add package Microsoft.EntityFrameworkCore
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
 mkdir DependencyInjection
 mkdir Mappers
 cd ..
